4f74319257eaf59dbb4bed0937f057afc636a87f
hilla2702 <hillali2702@gmail.com>
2024-06-19 21:23:45 +0300

    delete

--- files/Database_Simulator.py
+++
@@ -1,7 +0,0 @@
-from problems import HealthcareProblem
-from simulator import Simulator
-from  import your
-
-problem = HealthcareProblem()
-simulator = Simulator(your_planner, problem)
-result = simulator.rub(365*24)
